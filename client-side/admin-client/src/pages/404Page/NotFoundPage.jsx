4ddc9b803fb36cefb670fc8a5e49a58f4168ca48
Ngoc Khanh <khanhk56a12@gmail.com>
2023-04-06 08:08:06 +0700

    init admin client

---
+++ client-side/admin-client/src/pages/404Page/NotFoundPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss';
+
+const NotFoundPage = () => {
+  return <div className="not-found">404 error page</div>;
+};
+
+export default NotFoundPage;
---
+++ client-side/admin-client/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss';
+
+const Dashboard = () => {
+  return <section className="dashboard flex">dashboard</section>;
+};
+
+export default Dashboard;
